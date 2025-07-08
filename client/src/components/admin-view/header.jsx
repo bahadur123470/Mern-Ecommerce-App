2ec0490a2b8ab40b09e686f7fc9dfe2df7faa1cf
Bahadur Ali <bahadurali123470@gmail.com>
2025-07-08 10:22:57 +0500

    improved functionality

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { Button } from '../ui/button';
 import { AlignJustify, LogOut } from 'lucide-react';
 
-const AdminHeader = () => {
+const AdminHeader = ({ setOpen }) => {
     return (
-        <header className="flex items-center justify-between px-6 py-[20px] bg-foreground border-b shadow-sm">
+        <header className="flex items-center justify-between px-6 py-[20px] bg-background border-b shadow-sm">
             {/* Menu Button: Visible on small/medium, hidden on large screens */}
-            <Button variant="ghost" size="icon" className="lg:hidden">
+            <Button 
+            onClick={()=> setOpen(true)}
+            className="lg:hidden sm:block">
                 <AlignJustify className="h-5 w-5" />
                 <span className="sr-only">Toggle Menu</span>
             </Button>
             {/* Right-side Controls */}
             <div className="ml-auto">
-                <Button className="flex items-center gap-2 px-4 py-2 text-sm font-medium">
+                <Button className="flex items-center gap-3 px-8 py-8 text-sm font-medium">
                     <LogOut className="h-4 w-4" />
                     Logout
                 </Button>
--- client/src/components/admin-view/sidebar.jsx
+++ client/src/components/admin-view/sidebar.jsx
@@ -1,11 +1,7 @@
-import {
-    BadgeCheck,
-    ChartNoAxesCombined,
-    LayoutDashboard,
-    ShoppingBasket,
-} from 'lucide-react';
+import { BadgeCheck, ChartNoAxesCombined, LayoutDashboard, ShoppingBasket, } from 'lucide-react';
 import React, { Fragment } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from '../ui/sheet';
 
 const adminSidebarMenuItems = [
     {
@@ -28,7 +24,7 @@ const adminSidebarMenuItems = [
     },
 ];
 
-function MenuItems() {
+function MenuItems({ setOpen }) {
     const navigate = useNavigate();
     const location = useLocation();
 
@@ -40,7 +36,10 @@ function MenuItems() {
                 return (
                     <div
                         key={menuItem.id}
-                        onClick={() => navigate(menuItem.path)}
+                        onClick={() => {
+                            navigate(menuItem.path)
+                            setOpen ? setOpen(false) : null
+                        }}
                         className={`flex text-xl items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground cursor-pointer transition-colors ${
                             isActive
                                 ? 'bg-muted text-foreground'
@@ -56,17 +55,31 @@ function MenuItems() {
     );
 }
 
-const AdminSidebar = () => {
+const AdminSidebar = ({ open, setOpen}) => {
     const navigate = useNavigate();
 
     return (
         <Fragment>
+            <Sheet open={open} onOpenChange={setOpen} >
+                <SheetContent side='left' className="w-64">
+                    <div className='flex flex-col h-full'>
+                        <SheetHeader className="border-b">
+                            <SheetTitle className="flex gap-2 mt-5 mb-5">
+                                <ChartNoAxesCombined size={30} />
+                                <h1 className="text-3xl font-extrabold">Admin Panel</h1>
+                            </SheetTitle>
+                        </SheetHeader>
+                        <MenuItems setOpen={setOpen} />
+                    </div>
+                </SheetContent>
+
+            </Sheet>
             <aside className="flex w-[170px] flex-col border-r bg-background p-6">
                 <div
                     onClick={() => navigate('/admin/dashboard')}
                     className="flex cursor-pointer items-center gap-2"
                 >
-                    <ChartNoAxesCombined />
+                    <ChartNoAxesCombined size={30} />
                     <h1 className="text-3xl font-extrabold">Admin Panel</h1>
                 </div>
                 <MenuItems />
